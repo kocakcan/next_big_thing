65d95b442235b1ab21e00750272269470475658d
kocakcan <can_kocak1@epam.com>
2026-06-25 13:33:20 +0300

    scarfy's zooming now

--- src/scarfy.c
+++ src/scarfy.c
@@ -1,9 +1,11 @@
 #include "raylib.h"
 
-#define MAX_FRAME_SPEED	15
-#define MIN_FRAME_SPEED	1
-#define SCREEN_WIDTH	800
-#define SCREEN_HEIGHT	450
+#define MAX_FRAME_SPEED		60
+#define MIN_FRAME_SPEED		1
+#define FRAME_SPEED_STEP	5
+#define SPEED_BARS		(MAX_FRAME_SPEED / FRAME_SPEED_STEP)
+#define SCREEN_WIDTH		800
+#define SCREEN_HEIGHT		450
 
 int main(void) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Scarfy");
@@ -14,23 +16,28 @@ int main(void) {
 	Rectangle frameRec = { 0.0f, 0.0f, (float)scarfy.width / 6, 
 					   (float)scarfy.height };
 	int currentFrame = 0;
-	int framesCounter = 0;	/* # of spritesheet frames shown by second */
 	int framesSpeed = 8;
+	float frameTimer = 0.0f;
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
-		framesCounter++;
-		if (framesCounter >= (60 / framesSpeed)) {
-			framesCounter = 0;
+		float dt = GetFrameTime();
+		frameTimer += dt;
+		if (frameTimer >= (1.0f / framesSpeed)) {
+			frameTimer = 0.0f;
 			currentFrame++;
-			if (currentFrame > 5)	/* since we have 5 frames */
+			if (currentFrame > 5)
 				currentFrame = 0;
 			frameRec.x = (float)currentFrame * (float)scarfy.width / 6;
-		}	
-		if (IsKeyPressed(KEY_RIGHT)) framesSpeed++;
-		else if (IsKeyPressed(KEY_LEFT)) framesSpeed--;
-		if (framesSpeed > MAX_FRAME_SPEED) framesSpeed = MAX_FRAME_SPEED;
-		else if (framesSpeed < MIN_FRAME_SPEED) framesSpeed = MIN_FRAME_SPEED;
+		}
+		if (IsKeyPressed(KEY_RIGHT))
+			framesSpeed += FRAME_SPEED_STEP;
+		else if (IsKeyPressed(KEY_LEFT))
+			framesSpeed -= FRAME_SPEED_STEP;
+		if (framesSpeed > MAX_FRAME_SPEED)
+			framesSpeed = MAX_FRAME_SPEED;
+		else if (framesSpeed < MIN_FRAME_SPEED) 
+			framesSpeed = MIN_FRAME_SPEED;
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		DrawTexture(scarfy, 15, 40, WHITE);
@@ -40,10 +47,11 @@ int main(void) {
 		DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
 		DrawText(TextFormat("%02i FPS", framesSpeed), 575, 210, 10, DARKGRAY);
 		DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
-		for (int i = 0; i < MAX_FRAME_SPEED; ++i) {
-			if (i < framesSpeed)
-				DrawRectangle(250 + 21 * i, 205, 20, 20, RED);
-			DrawRectangleLines(250 + 21 * i, 205, 20, 20, MAROON);
+		int filledBars = framesSpeed / FRAME_SPEED_STEP;
+		for (int i = 0; i < SPEED_BARS; ++i) {
+			if (i < filledBars)
+				DrawRectangle(250 + 26 * i, 205, 20, 20, RED);
+			DrawRectangleLines(250 + 26 * i, 205, 20, 20, MAROON);
 		}
 		DrawTextureRec(scarfy, frameRec, position, WHITE); /* draw part of the texture */
 		EndDrawing();
